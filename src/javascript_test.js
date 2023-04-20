1b2120d838635c5b3bc493f04ec355434b063dc3
성현준 <hj@ec2-3-0-6-38.ap-southeast-1.compute.amazonaws.com>
2023-04-21 01:46:02 +0900

    javascript 테스트, launch.json 파일을 추가하여 debug 진행

---
+++ src/javascript_test.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+var numCounters = 0
+
+function getCounter() {
+    numCounters += 1
+
+    var result = { count: count, total:0 }
+    function count() {
+        result.count += 1
+    }
+    return result
+}
+
+var counterA = getCounter()
+counterA.count()
+counterA.count()
+
+var counterB = getCounter()
+counterB.count()
+
+console.log(counterA.total, counterB.total, numCounters)
